5204afb31770dd6db85034ac1270aeff8a38e868
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2022-07-27 07:17:28 -0500

    Upload Button - Home Activity

--- Android APP/app/src/main/java/com/example/photos_app/AuthActivity.kt
+++ Android APP/app/src/main/java/com/example/photos_app/AuthActivity.kt
@@ -1,20 +1,13 @@
 package com.example.photos_app
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AlertDialog
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
-
-    private val GOOGLE_SIGN_IN = 100
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
@@ -22,36 +15,20 @@ class AuthActivity : AppCompatActivity() {
         //Setup
 
         setup()
-        session()
 
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        authLayout.visibility = View.VISIBLE
-    }
-
-    private fun session() {
-        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
-        val email = prefs.getString("email", null)
-        val provider = prefs.getString("provider", null)
-
-        if(email != null && provider != null) {
-            authLayout.visibility = View.INVISIBLE
-            showHome(email, ProviderType.valueOf(provider))
-        }
     }
 
     private fun setup() {
 
-        title = "Autenticación"
-        signUpButton.setOnClickListener{
-            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+        title = "Authentication"
+        signUpButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
                 FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(emailEditText.text.toString(),
-                        passwordEditText.text.toString()).addOnCompleteListener{
-                        if (it.isSuccessful){
+                    .createUserWithEmailAndPassword(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
                             showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
                         } else {
                             showAlert()
@@ -73,23 +50,13 @@ class AuthActivity : AppCompatActivity() {
                     }
             }
         }
-        googleButton.setOnClickListener{
-            // Configuracion GOOGLE
-
-            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
-            val googleClient = GoogleSignIn.getClient(this, googleConf)
-
-        }
     }
 
     private fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
-        builder.setMessage("Se ha producido un error autenticando al usuario")
-        builder.setPositiveButton("Aceptar", null)
+        builder.setMessage("An error has been occurred during the user authentication process")
+        builder.setPositiveButton("Accept", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
--- Android APP/app/src/main/java/com/example/photos_app/HomeActivity.kt
+++ Android APP/app/src/main/java/com/example/photos_app/HomeActivity.kt
@@ -1,50 +1,132 @@
 package com.example.photos_app
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
+
 enum class ProviderType {
-    BASIC,
-    GOOGLE
+    BASIC
 }
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener,
+    RecyclerAdaptor.CountOfImagesWhenRemoved {
+    var recyclerView: RecyclerView? = null
+    var textView: TextView? = null
+    //var textView_msg: TextView? = null
+    var button: Button? = null
+    var button_remove: Button? = null
+    var list: ArrayList<Uri>? = null
+    var adaptor: RecyclerAdaptor? = null
+    var colum = arrayOf(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        //Setup
-
+        list = ArrayList()
+        recyclerView = findViewById(R.id.recycler)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+        button_remove = findViewById(R.id.button_remove)
+        adaptor = RecyclerAdaptor(list!!, applicationContext, this)
+        recyclerView?.layoutManager = GridLayoutManager(this@HomeActivity, 4)
+        recyclerView?.adapter = adaptor
+        button?.setOnClickListener(this)
+        button_remove?.setOnClickListener(this)
+        if (ActivityCompat.checkSelfPermission(
+                this, colum[0]
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this, colum[1]
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(colum, 123)
+            }
+        }
         val bundle: Bundle? = intent.extras
         val email: String? = bundle?.getString("email")
         val provider: String? = bundle?.getString("provider")
         setup(email?: "", provider?: "")
-
-        //Guardar Datos
-
-        val prefs = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
-        prefs.putString("email", email)
-        prefs.putString("provider",provider)
-        prefs.apply()
     }
 
     private fun setup(email: String, provider: String) {
-        title = "Inicio"
+        title = "Home"
         emailTextView.text = email
         providerTextView.text = provider
-
-        //Borrando Datos
-
-        val prefs = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
-        prefs.clear()
-        prefs.apply()
 
         logOutButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button -> openGalley()
+            R.id.button_remove -> {
+                list!!.clear()
+                adaptor?.notifyDataSetChanged()
+                textView!!.text = "Image(" + list!!.size + ")"
+            }
+        }
+    }
+
+    private fun openGalley() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Selcet Picture"), 123)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123 && resultCode == RESULT_OK) {
+            if (data!!.clipData != null) {
+                val x = data.clipData!!.itemCount
+                for (i in 0 until x) {
+                    if (list!!.size <= 7) {
+                        list!!.add(data.clipData!!.getItemAt(i).uri)
+                    }
+                    else (showAlertImg())
+
+                    adaptor?.notifyDataSetChanged()
+                    textView!!.text = "Image(" + list!!.size + ")"
+                }
+            if (data.data != null) {
+                val imgurl = data.data!!.path
+                list!!.add(Uri.parse(imgurl))
+                }
+            }
+        }
+    }
+
+    override fun clicked(getSize: Int) {
+        textView!!.text = "Image(" + list!!.size + ")"
+    }
+    private fun showAlertImg() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Amount of images exceded (> 8)")
+        builder.setPositiveButton("Accept", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
