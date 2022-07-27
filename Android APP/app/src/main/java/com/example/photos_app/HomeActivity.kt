3eb06b367887e8ded826e71c32c444add679abe3
Jean Camacho <71253306+JeanN00B@users.noreply.github.com>
2022-07-26 23:18:58 -0500

    Revert "Google Auth - Por terminar"

--- Android APP/app/src/main/java/com/example/photos_app/HomeActivity.kt
+++ Android APP/app/src/main/java/com/example/photos_app/HomeActivity.kt
@@ -1,14 +1,12 @@
 package com.example.photos_app
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 enum class ProviderType {
-    BASIC,
-    GOOGLE
+    BASIC
 }
 
 class HomeActivity : AppCompatActivity() {
@@ -22,25 +20,12 @@ class HomeActivity : AppCompatActivity() {
         val email: String? = bundle?.getString("email")
         val provider: String? = bundle?.getString("provider")
         setup(email?: "", provider?: "")
-
-        //Guardar Datos
-
-        val prefs = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
-        prefs.putString("email", email)
-        prefs.putString("provider",provider)
-        prefs.apply()
     }
 
     private fun setup(email: String, provider: String) {
         title = "Inicio"
         emailTextView.text = email
         providerTextView.text = provider
-
-        //Borrando Datos
-
-        val prefs = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
-        prefs.clear()
-        prefs.apply()
 
         logOutButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
