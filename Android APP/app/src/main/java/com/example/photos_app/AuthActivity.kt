52bea980fc2a8a635bc65464246c2a7418e0edaf
Jean Carlo Camacho <jean.camacho@yachaytech.edu.ec>
2022-07-28 22:03:40 -0500

    other way of trying to login with google
    Testing something form a tutorial of login with google from 2022 in kothlin and it is pretty different of what we have now, but I haven't get the solution yet :c

--- Android APP/app/src/main/java/com/example/photos_app/AuthActivity.kt
+++ Android APP/app/src/main/java/com/example/photos_app/AuthActivity.kt
@@ -1,26 +1,49 @@
 package com.example.photos_app
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.util.RetainForClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_auth.*
 
+enum class ProviderType{
+    BASIC,
+    GOOGLE
+}
+
 class AuthActivity : AppCompatActivity() {
+    private val GOOGLE_SIGN_IN = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
         //Setup
+        val bundle = intent.extras
+        val email = bundle?.getString(key:"email")
+        val provider = bundle?.getString(key:"provider")
+        setup(email ?: "", provider ?: "")
 
-        setup()
-
+        //Save users data
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
     }
 
-    private fun setup() {
+    private fun setup(email: String, provider: String) {
 
         title = "Authentication"
+
+
         signUpButton.setOnClickListener {
             if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
                 FirebaseAuth.getInstance()
@@ -50,6 +73,16 @@ class AuthActivity : AppCompatActivity() {
                     }
             }
         }
+        googleButton.setOnClickListener{
+            //Code that need to be debugged
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInInternet, GOOGLE_SIGN_IN)
+        }
     }
 
     private fun showAlert() {
@@ -67,5 +100,28 @@ class AuthActivity : AppCompatActivity() {
             putExtra("provider", provider.name)
         }
         startActivity(homeIntent)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null){
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                    if (it.isSuccessful){
+                        showHome(account.email ?: "", ProviderType.GOOGLE)
+                    } else {
+                        showAlert()
+                    }
+                }
+            } catch (e: ApiException){
+                showAlert()
+            }
+
+        }
     }
 }
