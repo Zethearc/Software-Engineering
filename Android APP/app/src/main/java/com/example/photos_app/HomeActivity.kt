c19fdcc303d1f1826ab76b0a4e3ef472d7992dc7
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2022-08-01 03:51:27 -0500

    Cambios menores

--- Android APP/app/src/main/java/com/example/photos_app/HomeActivity.kt
+++ Android APP/app/src/main/java/com/example/photos_app/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
