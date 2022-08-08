53ca1c43e663d06af6c6ccc25e49c59a083b0cc2
Stevendlc99 <stevendlc99@gmail.com>
2022-08-07 22:50:05 -0500

    creando mibranch

---
+++ Android APP/app/src/main/java/net/simplifiedcoding/imageuploader/MyAPI.kt
@@ -0,0 +1,47 @@
+package net.simplifiedcoding.imageuploader
+
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import java.util.concurrent.TimeUnit
+
+
+interface MyAPI {
+
+    @Multipart
+    @POST("captionmodel")
+    fun uploadImage(
+        @Part image: MultipartBody.Part,
+        @Part("desc") desc: RequestBody
+    ): Call<UploadResponse>
+
+    companion object {
+        operator fun invoke(): MyAPI {
+            return Retrofit.Builder()
+                .baseUrl("http://192.168.0.7:5000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(getClient())
+                .build()
+                .create(MyAPI::class.java)
+
+        }
+        private fun getClient(): OkHttpClient {
+            return OkHttpClient.Builder()
+                .callTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120,TimeUnit.SECONDS)
+                .connectTimeout(120,TimeUnit.SECONDS)
+                .build()
+        }
+    }
+
+
+
+
+
+}
