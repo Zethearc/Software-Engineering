d8cb5c775c1d8f84063b0b45224fe1922e9a4dcd
Jean Carlo Camacho <jean.camacho@yachaytech.edu.ec>
2022-08-13 12:11:18 -0500

    Piechart
    Sentiment branch adaptation to generate the visual implementation of a piechart

--- Android APP/app/src/main/java/com/example/photos_app/SentimentModel.kt
+++ Android APP/app/src/main/java/com/example/photos_app/SentimentModel.kt
@@ -1,33 +1,33 @@
 package com.example.photos_app
 
 import android.content.ContentValues.TAG
-
-import androidx.appcompat.app.AppCompatActivity
-import org.tensorflow.lite.task.text.nlclassifier.BertNLClassifier
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.EditText
+import android.widget.ScrollView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.PieChart
 import com.google.android.gms.tasks.Continuation
-import com.google.firebase.ml.custom.FirebaseCustomRemoteModel
+import com.google.android.gms.tasks.Task
 import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions
 import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.Task
+import com.google.firebase.ml.custom.FirebaseCustomRemoteModel
+import org.tensorflow.lite.task.text.nlclassifier.BertNLClassifier
 import java.io.File
-import java.lang.Exception
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 
-
-
 class SentimentModel : AppCompatActivity() {
     private lateinit var resultTextView: TextView
-    private lateinit var inputEditText: EditText
+    private lateinit var inputImageCaptioning: EditText
     private lateinit var executorService: ExecutorService
     private lateinit var scrollView: ScrollView
-    private lateinit var predictButton: Button
+    //piechart
+    lateinit var pieChart: PieChart
 
     // TODO 5: Define a NLClassifier variable
     private lateinit var textClassifier: BertNLClassifier
@@ -36,48 +36,35 @@ class SentimentModel : AppCompatActivity() {
         setContentView(R.layout.sentiment_model)
         Log.v(TAG, "onCreate")
         executorService = Executors.newSingleThreadExecutor()
-        resultTextView = findViewById(R.id.result_text_view)
-        inputEditText = findViewById(R.id.input_text)
-        scrollView = findViewById(R.id.scroll_view)
-        predictButton = findViewById(R.id.predict_button)
+        // TODO JEAN: Changed the var 'inputEditText' to "inputImageCaptioning" because will collect the captioning from the Franklin model, by the moment has an test array
+        val inputImageCaptioning = arrayOf("Hello world","I hate you","I love you","You look awesome","You are beautiful")
 
-        predictButton.setOnClickListener(
-            View.OnClickListener { v: View -> classify(inputEditText.getText().toString()) })
+        classify(inputImageCaptioning.toString())
+        // JEAN: need to pass that classify to an new array that append
 
         // TODO 3: Call the method to download TFLite model
         downloadModel("MobileBERT")
     }
 
-    /** Send input text to TextClassificationClient and get the classify messages.  */
-    private fun classify(text: String) {
+    /** Send input text to TextClassificationClient and get the classify into 3 lists.  */
+    private fun classify(arrayText: Array) {
         executorService.execute {
 
             // TODO 7: Run sentiment analysis on the input text
-            val results = textClassifier.classify(text)
-
-            // Create an array to predict sentiment results
-            //val names = arrayOf("Hello world","I hate you","I love you","You look awesome","You are beautiful")
-
-            // TODO 8: Convert the result to a human-readable text
-            var textToShow = "Input: $text\nOutput:\n"
-            //Arreglo para almacenar el resultado
-            //var texts = arrayOf<String>()
-            //Inicion del bucle para mostrar los resultados
-            for (i in results.indices) {
-                val result = results[i]
-                textToShow += String.format("    %s: %s\n", result.label, result.score)
-                //texts[i] = String.format("    %s", result.score)
+            var negativeValues: Float
+            var neutralValues: Float
+            var positiveValues: Float
+            //Append negative, neutral and positive values on the 3 vars, then pass to the pie chart
+            for (element in arrayText.size) {
+                val results = textClassifier.classify(arrayText[element])
+                for (i in results.indices) {
+                    negativeValues += (results[0].score * 100f)
+                    neutralValues += (results[1].score * 100f)
+                    positiveValues += (results[2].score * 100f)
+                }
             }
-            //for(i in 0..2){
-            //    val resultados = String.format("     %s\n", texts[i])
-            //    showResult(resultados)
-            //}
-            textToShow += "-----------\n"
-
-            // Show classification result on screen
-            showResult(textToShow)
-            //showResult(texts.toString())
         }
+
     }
 
     /** Show classification result on the screen.  */
@@ -89,7 +76,7 @@ class SentimentModel : AppCompatActivity() {
             resultTextView.append(textToShow)
 
             // Clear the input text.
-            inputEditText.text.clear()
+            inputImageCaptioning.text.clear()
 
             // Scroll to the bottom to show latest entry's classification result.
             scrollView.post { scrollView.fullScroll(View.FOCUS_DOWN) }
@@ -118,9 +105,6 @@ class SentimentModel : AppCompatActivity() {
 
                 // TODO 6: Initialize a TextClassifier with the downloaded model
                 textClassifier = BertNLClassifier.createFromFile(modelFile)
-
-                // Enable predict button
-                predictButton.isEnabled = true
                 null
             })
             .addOnFailureListener { e: Exception? ->
@@ -131,7 +115,59 @@ class SentimentModel : AppCompatActivity() {
                     Toast.LENGTH_LONG
                 )
                     .show()
-                predictButton.isEnabled = false
             }
     }
+
+    // TODO #Piechart section#
+    // on below line we are initializing our
+    // variable with their ids.
+    pieChart = findViewById(R.id.pieChart)
+
+    // on below line we are setting user percent value,
+    // setting description as enabled and offset for pie chart
+    pieChart.setUsePercentValues(true)
+    pieChart.getDescription().setEnabled(false)
+    pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+
+    // on below line we are setting drag for our pie chart
+    pieChart.setDragDecelerationFrictionCoef(0.95f)
+
+    // on below line we are setting hole
+    // and hole color for pie chart
+    pieChart.setDrawHoleEnabled(true)
+    pieChart.setHoleColor(Color.WHITE)
+
+    // on below line we are setting circle color and alpha
+    pieChart.setTransparentCircleColor(Color.WHITE)
+    pieChart.setTransparentCircleAlpha(110)
+
+    // on  below line we are setting hole radius
+    pieChart.setHoleRadius(58f)
+    pieChart.setTransparentCircleRadius(61f)
+
+    // on below line we are setting center text
+    pieChart.setDrawCenterText(true)
+
+    // on below line we are setting
+    // rotation for our pie chart
+    pieChart.setRotationAngle(0f)
+
+    // enable rotation of the pieChart by touch
+    pieChart.setRotationEnabled(true)
+    pieChart.setHighlightPerTapEnabled(true)
+
+    // on below line we are setting animation for our pie chart
+    pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+    // on below line we are disabling our legend for pie chart
+    pieChart.legend.isEnabled = false
+    pieChart.setEntryLabelColor(Color.WHITE)
+    pieChart.setEntryLabelTextSize(12f)
+
+    // on below line we are creating array list and
+    // adding data to it to display in pie chart
+
+    entries.add(negativeValues)
+    entries.add(neutralValues)
+    entries.add(positiveValues)
 }
